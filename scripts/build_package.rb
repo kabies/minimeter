9507cffecc45b5c730384f873b19a0a523117523
kabies <37025477+kabies@users.noreply.github.com>
2018-04-17 04:29:39 +0900

    add SDL license

--- scripts/build_package.rb
+++ scripts/build_package.rb
@@ -43,6 +43,27 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 EOS
 
+SDL=<<EOS
+Simple DirectMedia Layer
+Copyright (C) 1997-2018 Sam Lantinga <slouken@libsdl.org>
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+EOS
+
 class License
   attr_accessor :gemname, :license, :author
 end
@@ -93,6 +114,8 @@ licenses.reject{|l| l.author.end_with? "mruby developers" }.each do |license|
   end
 end
 
+f.puts "# SDL2 2.0.8"
+f.puts SDL
 f.close
 
 `rm minimeter.zip`
